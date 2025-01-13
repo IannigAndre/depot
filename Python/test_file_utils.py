bc144544ed043cad1ca53674a3e99bdea73d6192
IannigAndre <andre.i1@stjolorient.fr>
2025-01-13 16:21:41 +0100

    2

--- Python/test_file_utils.py
+++ Python/test_file_utils.py
@@ -27,6 +27,7 @@ class TestFileUtils(unittest.TestCase):
     def testAreSame(self):
         """Cas de test vérifiant que les fichiers sont identiques."""
         assert not file_utils.diff(self.file_first_name, self.file_first_name), "similitude non détectée"
+        assert file_same.diff(self.file_first_name, self.file_first_name), "similitude non détectée"
     
     def testAreDifferent(self):
         """Cas de test -  fichiers différents."""
