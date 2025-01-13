baae89a7a206f179b5b8d2de8192adff52566930
IannigAndre <andre.i1@stjolorient.fr>
2025-01-13 16:31:39 +0100

    3

--- Python/file_utils.py
+++ Python/file_utils.py
@@ -2,6 +2,8 @@
 
 def diff(file_first, file_second):
     """Fonction retournant True si deux fichiers sont différents."""
+    with open (mon_fichier1,encoding="utf-8") as file1_id:
+		with open (
     return True
 
 def same(file_first, file_second):
